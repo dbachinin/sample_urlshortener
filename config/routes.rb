5cfe3e212f7ea07d5ab58f86bcdea347db142656
tg-it-03 <dbachinin@gmail.com>
2017-04-28 08:33:14 +0300

    5razz2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'persons/profile'
+
+  resources :links
+  resources :users
+
+  get ':slug' => 'links#show'
+  #get  '/signup',  to: 'users#new'
+
+  #get '/all' => 'home#all'
+  root 'links#index'
+  get 'persons/profile', as: 'user_root'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
