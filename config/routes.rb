f275d7977992b408a5e1b7e29033112f4a6f78ef
dbachinin <dbachinin@ya.ru>
2017-05-12 13:50:39 +0300

    rewrigth head_dont worked email

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'persons/profile'
 
   resources :links
@@ -10,6 +11,10 @@ Rails.application.routes.draw do
   #get '/all' => 'home#all'
   root 'links#index'
   get 'persons/profile', as: 'user_root'
+
+delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
+get '/sign_in', to: 'user_sessions#new', as: :sign_in
+resources :password_resets, only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
